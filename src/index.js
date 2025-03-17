6a87d5619bd85883502d3ee26763fd1fd06b354f
iSitanshu <Sitanshumishra18@gmail.com>
2025-03-18 02:52:44 +0530

    DB connected

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// require('dotenv').config({path: './env'})
+import dotenv from 'dotenv'
+import mongoose from "mongoose";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
