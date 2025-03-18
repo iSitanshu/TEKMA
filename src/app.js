c60481d2bfdeca18dc88f1d195b486a5421c5dfc
iSitanshu <Sitanshumishra18@gmail.com>
2025-03-18 20:16:28 +0530

    Backend Code

--- src/app.js
+++ src/app.js
@@ -1,27 +1,26 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-const app = express()
+const app = express();
 
 const corsOptions = {
-    origin: "http://localhost:5173",
-    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-    credentials: true,
-}
+  origin: 'http://localhost:5173',
+  methods: 'GET, POST, PUT, DELETE, PATCH, HEAD',
+  credentials: true,
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.static('public'));
+app.use(cookieParser());
 
+// Routes imports
+import userRouter from "./routes/user.routes.js"
 
-//routes imports
-import userRouter from './routes/user.routes.js'
-
-//routes declaration
+// Routes declaration
 app.use("/api/v1/users", userRouter)
 
 // http://localhost:8000/api/v1/users/register
